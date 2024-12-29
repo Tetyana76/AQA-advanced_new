6e0e7d2365be912f575770a9b04d05155d315da5
Tetiana Klapchenko <tklapchenko@gmail.com>
2024-12-29 12:23:31 +0200

    remove login.spec

--- cypress/e2e/tests/loginFunction.spec.js
+++
@@ -1,9 +0,0 @@
-
-describe('Login Tests', () => {
-    const testEmail = "tklapchenko+1@gmail.com";
-    const testPassword = "Password123";
-    it.only('should log in using custom command', () => {
-        cy.login(testEmail, testPassword);
-    });
-});
-    
